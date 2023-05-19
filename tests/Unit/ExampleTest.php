9d8fe5193567c5b83b483c6d8c3a824f90516925
JesusAnd <jesusandradesustaita@gmail.com>
2023-05-18 22:05:11 -0600

    update test

--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -14,6 +14,6 @@ class ExampleTest extends TestCase
         $this->assertTrue(true);
     }
     public function testing(){
-        $this->assertEquals(1,1);
+        $this->assertEquals(5,1);
        }
 }
